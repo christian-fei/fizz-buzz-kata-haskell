c10bafa07689006eb14a1b3d9f1955ca1d36a7da
Christian Fei <crifei93@gmail.com>
2015-02-19 23:44:46 +0100

    made 15 = FizzBuzz pass

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -4,7 +4,7 @@ where
 calcFizzBuzz :: Int -> String
 calcFizzBuzz 0 = "0"
 calcFizzBuzz n
-  | n == 15             = "FizzBuzz"
-  | n `mod` 5 == 0      = "Buzz"
-  | n `mod` 3 == 0      = "Fizz"
+  | n `mod` 15 == 0     = "FizzBuzz"
+  | n `mod` 5  == 0     = "Buzz"
+  | n `mod` 3  == 0     = "Fizz"
   | otherwise           = show n
