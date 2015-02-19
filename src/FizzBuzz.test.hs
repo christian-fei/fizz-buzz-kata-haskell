5321cff6fc3743b18b2268fe09e61b20f3ce862d
Christian Fei <crifei93@gmail.com>
2015-02-19 23:33:49 +0100

    removed depreceated tests for 5 = Buzz

--- src/FizzBuzz.test.hs
+++ src/FizzBuzz.test.hs
@@ -14,14 +14,12 @@ normalNumbers = [
   calcFizzBuzz 0 ~?= "0",
   calcFizzBuzz 1 ~?= "1",
   calcFizzBuzz 2 ~?= "2",
-  calcFizzBuzz 4 ~?= "4",
-  calcFizzBuzz 5 ~?= "5"
+  calcFizzBuzz 4 ~?= "4"
   ]
 
 multipleOfThreeIsFizz = [
   calcFizzBuzz 3  ~?= "Fizz",
   calcFizzBuzz 6  ~?= "Fizz",
   calcFizzBuzz 9  ~?= "Fizz",
-  calcFizzBuzz 12 ~?= "Fizz",
-  calcFizzBuzz 15 ~?= "Fizz"
+  calcFizzBuzz 12 ~?= "Fizz"
   ]
