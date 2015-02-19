bc76ee82f22dbd81f740b4b9a7c7b0a8ba8a1713
Christian Fei <crifei93@gmail.com>
2015-02-19 23:30:38 +0100

    made multiple of 3 = Fizz pass, added special case for 0

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -2,6 +2,7 @@ module FizzBuzz
 where
 
 calcFizzBuzz :: Int -> String
+calcFizzBuzz 0 = "0"
 calcFizzBuzz n
-  | n == 3      = "Fizz"
-  | otherwise   = show n
+  | n `mod` 3 == 0      = "Fizz"
+  | otherwise           = show n
