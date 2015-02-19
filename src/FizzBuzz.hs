50a9613c1f328ded3aed11d8ba0e12e45615eb5c
Christian Fei <crifei93@gmail.com>
2015-02-20 00:17:04 +0100

    Revert "Adding 0 as base case"

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -3,8 +3,8 @@ where
 
 
 fizzbuzz :: Int -> String
-fizzbuzz 0 = 0
 fizzbuzz n
+  | n == 0           = show n
   | n `mod` 15 == 0  = "FizzBuzz"
   | n `mod` 3 == 0   = "Fizz"
   | n `mod` 5 == 0   = "Buzz"
