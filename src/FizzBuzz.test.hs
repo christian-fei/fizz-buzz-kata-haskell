99173e23d79bb14517b1330b352f271ccb5731b0
Christian Fei <crifei93@gmail.com>
2015-02-19 23:39:31 +0100

    refactoring tests

--- src/FizzBuzz.test.hs
+++ src/FizzBuzz.test.hs
@@ -7,14 +7,7 @@ import FizzBuzz
 main = do runTestTT tests
 
 tests = TestList $
-  normalNumbers ++ multipleOfThreeIsFizz ++ [
-    calcFizzBuzz 5 ~?= "Buzz",
-    calcFizzBuzz 10 ~?= "Buzz",
-    calcFizzBuzz 15 ~?= "Buzz",
-    calcFizzBuzz 20 ~?= "Buzz",
-    calcFizzBuzz 25 ~?= "Buzz",
-    calcFizzBuzz 30 ~?= "Buzz"
-    ]
+  normalNumbers ++ multipleOfThreeIsFizz ++ multipleOfFiveIsBuzz
 
 
 normalNumbers = [
@@ -29,4 +22,13 @@ multipleOfThreeIsFizz = [
   calcFizzBuzz 6  ~?= "Fizz",
   calcFizzBuzz 9  ~?= "Fizz",
   calcFizzBuzz 12 ~?= "Fizz"
+  ]
+
+multipleOfFiveIsBuzz = [
+  calcFizzBuzz 5 ~?= "Buzz",
+  calcFizzBuzz 10 ~?= "Buzz",
+  calcFizzBuzz 15 ~?= "Buzz",
+  calcFizzBuzz 20 ~?= "Buzz",
+  calcFizzBuzz 25 ~?= "Buzz",
+  calcFizzBuzz 30 ~?= "Buzz"
   ]
