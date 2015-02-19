a5de15ce3f27838c5ff3165a96866d92a1485719
Christian Fei <crifei93@gmail.com>
2015-02-19 19:52:58 +0100

    passing 15 = FizzBuzz

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -5,6 +5,7 @@ where
 fizzbuzz :: Int -> String
 fizzbuzz n
   | n == 0           = show n
+  | n `mod` 15 == 0  = "FizzBuzz"
   | n `mod` 3 == 0   = "Fizz"
   | n `mod` 5 == 0   = "Buzz"
   | otherwise        = show n
