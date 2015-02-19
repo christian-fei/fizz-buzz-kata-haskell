ef258466ab91403d261d66cfc209185d2ded0724
Christian Fei <crifei93@gmail.com>
2015-02-19 23:37:23 +0100

    fixing priority of Buzz of Fizz

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -4,6 +4,6 @@ where
 calcFizzBuzz :: Int -> String
 calcFizzBuzz 0 = "0"
 calcFizzBuzz n
-  | n `mod` 3 == 0      = "Fizz"
   | n `mod` 5 == 0      = "Buzz"
+  | n `mod` 3 == 0      = "Fizz"
   | otherwise           = show n
