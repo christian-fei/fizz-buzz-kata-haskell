84bd4a8e33b6086cd266f32024ecbd60d2787e0a
Christian Fei <crifei93@gmail.com>
2015-02-21 00:29:55 +0100

    refactored *15 = FizzBuzz code

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -6,9 +6,8 @@ where
 
 fizzbuzz :: Int -> String
 fizzbuzz 0 = "0"
-fizzbuzz 15 = "FizzBuzz"
-fizzbuzz 30 = "FizzBuzz"
 fizzbuzz n
+  | n `rem` 15 == 0   = "FizzBuzz"
   | n `rem` 5 == 0    = "Buzz"
   | n `rem` 3 == 0    = "Fizz"
   | otherwise         = show n
